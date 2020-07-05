41a1634e9f490d883063cdd82618d6d632402e16
Hector Vasquez <hvasquezdev@gmail.com>
2020-07-04 22:26:17 -0400

    Refactor card component

---
+++ components/BaseComponents/BaseCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    :class="classNames"
+    class="base-card font-manrope text-base flex items-center justify-center"
+  >
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+
+  props: {
+    shadow: {
+      type: Boolean,
+      default: true,
+    },
+    rounded: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  computed: {
+    classNames() {
+      return {
+        'shadow-card': this.shadow,
+        'rounded-md': this.rounded,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+.base-card {
+  padding: 18px 20px;
+}
+</style>
--- components/Home/BrowseJobs.vue
+++ components/Home/BrowseJobs.vue
@@ -1,63 +1,55 @@
 <template>
-  <section class="browse-jobs-section flex flex-col items-center">
+  <section class="browse-jobs flex flex-col items-center">
     <!-- Section Title -->
-    <h1 class="text-black font-normal font-manrope">
+    <h1 class="text-dark-secondary font-normal font-manrope">
       Browse Jobs by <span class="font-extrabold">Area / Department</span>
     </h1>
     <!-- Tags Container -->
-    <div class="jobs-container grid grid-cols-4">
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
-      <card> Lorem Ipsum Dolor</card>
+    <div class="jobs-container grid">
+      <base-card v-for="card in 16" :key="card" class="w-full">
+        Lorem Ipsum Dolor
+      </base-card>
     </div>
     <!-- See All Button -->
     <base-button shadow class="see-all-button" color="danger" rounded>
-      See All</base-button
-    >
+      See All
+    </base-button>
   </section>
 </template>
 
 <script>
-import Card from '@/components/Card';
+import BaseCard from '@/components/BaseComponents/BaseCard';
 import BaseButton from '@/components/BaseComponents/BaseButton';
 
 export default {
   name: 'BrowseJobs',
   components: {
-    Card,
+    BaseCard,
     BaseButton,
   },
 };
 </script>
 
 <style>
-.browse-jobs-section {
+.browse-jobs {
   padding: 108px 0 78px;
 }
-.browse-jobs-section .see-all-button {
+.browse-jobs .see-all-button {
   width: 216px;
   height: 62px;
+  font-size: 18px;
 }
-.jobs-container {
+.browse-jobs .jobs-container {
   max-width: 1140px;
-  gap: 22px 40px;
+  gap: 25px 40px;
   margin-top: 64px;
   margin-bottom: 56px;
+  grid-template-columns: repeat(4, 255px);
 }
-.browse-jobs-section h1 {
+.browse-jobs .jobs-container .base-card {
+  max-width: 255px;
+}
+.browse-jobs h1 {
   font-size: 45px;
 }
 </style>
