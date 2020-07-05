5163b415bd0713e4d40125ef518974024859b54d
angeleraser <angeleraser@gmail.com>
2020-07-05 11:34:43 -0400

    Add latest news

---
+++ components/CardBlog.vue
@@ -0,0 +1,84 @@
+<template>
+  <article class="card-blog flex items-start shadow-card font-manrope">
+    <!-- Image -->
+    <img
+      src="/images/jpg/card-blog-placeholder.jpg"
+      alt="Blog image"
+      class="shadow-card"
+    />
+    <!-- Data -->
+    <section class="data self-center">
+      <!-- Title -->
+      <h2 class="card-blog-title text-left text-dark-secondary">
+        Anual Impact Report
+      </h2>
+      <!-- Date, tag -->
+      <div
+        class="text-base w-full self-center flex text-grey-light-2 flex items-center mt-4 mb-8"
+      >
+        <p class="date">June 15, 2020</p>
+        <div class="divisory-line"></div>
+        <p class="tag text-purple-primary">
+          Workplace
+        </p>
+      </div>
+      <!--Description-->
+      <p class="card-blog-description text-base text-grey-light-2">
+        Freelancers release new study revealing insights into the almost 54
+        million people freelancing in America innovations aimed.
+      </p>
+      <!-- Read more button -->
+      <base-button
+        class="read-more-button border border-solid shadow-btn"
+        rounded
+        color="white"
+      >
+        Read Article</base-button
+      >
+    </section>
+  </article>
+</template>
+
+<script>
+import BaseButton from '@/components/BaseComponents/BaseButton';
+
+export default {
+  name: 'CardBlog',
+  components: {
+    BaseButton,
+  },
+};
+</script>
+
+<style>
+.card-blog {
+  width: 100%;
+  max-width: 1140px;
+  padding: 20px;
+  border-radius: 15px;
+}
+.card-blog h2 {
+  font-size: 25px;
+}
+.card-blog .card-blog-description {
+  margin: 28px 0 34px;
+}
+.card-blog .divisory-line {
+  height: 25px;
+  border: 0.5px solid #d6d6d6;
+  margin: 0 22px;
+}
+.card-blog img {
+  height: 338px;
+  border-radius: 15px;
+  width: 542px;
+}
+.card-blog .data {
+  padding-left: 46px;
+}
+
+.card-blog .read-more-button {
+  height: 56px;
+  width: 178px;
+}
+</style>
---
+++ components/Home/LatestNews.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="latest-news flex flex-col items-center font-manrope">
+    <!-- Title -->
+    <h1 class="text-black font-normal">
+      Latest <span class="font-extrabold">Blog News</span>
+    </h1>
+    <card-blog />
+    <!--- Small cards --->
+    <div class="card-blog-small-container flex items-start">
+      <card-blog-small />
+      <card-blog-small />
+      <card-blog-small />
+    </div>
+  </section>
+</template>
+
+<script>
+import CardBlog from '@/components/CardBlog';
+import CardBlogSmall from '@/components/CardBlogSmall';
+export default {
+  name: 'LatestNews',
+  components: {
+    CardBlog,
+    CardBlogSmall,
+  },
+};
+</script>
+
+<style>
+.latest-news {
+  padding: 110px 0 84px;
+}
+.latest-news h1 {
+  font-size: 45px;
+  margin-bottom: 58px;
+}
+.latest-news .card-blog-title,
+.date {
+  font-weight: 400; /* regular */
+}
+.latest-news .card-blog-description {
+  font-weight: 500; /* medium*/
+}
+.latest-news .tag {
+  font-weight: 600; /* semibold*/
+}
+.latest-news .card-blog {
+  margin-bottom: 30px;
+}
+.latest-news .card-blog-small:not(:last-child) {
+  margin-right: 30px;
+}
+</style>
