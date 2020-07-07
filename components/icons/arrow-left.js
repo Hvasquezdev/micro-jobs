802272fdbf2a11589f65d52ecd63f09153435431
Hector Vasquez <hvasquezdev@gmail.com>
2020-07-07 11:32:53 -0400

    Update icons

--- components/icons/arrow-left.js
+++ components/icons/arrow-left.js
@@ -5,6 +5,6 @@ icon.register({
     width: 10.781,
     height: 19.75,
     viewBox: '0 0 10.781 19.75',
-    data: '<defs><filter id="svgicon_arrow-left_a" x="432.594" y="412.125" width="10.781" height="19.75" filterUnits="userSpaceOnUse"><feFlood result="flood" flood-color="#fff"/><feComposite result="composite" operator="in" in2="SourceGraphic"/><feBlend result="blend" in2="SourceGraphic"/></filter></defs><path pid="0" data-name="Forma 1" d="M432.888 422.694l8.8 8.888a.981.981 0 001.4 0 1 1 0 000-1.41l-8.1-8.183 8.1-8.183a1.006 1.006 0 000-1.412.984.984 0 00-1.4 0l-8.8 8.888a1.016 1.016 0 000 1.412z" transform="translate(-432.594 -412.125)" fill-rule="evenodd" filter="url(#svgicon_arrow-left_a)"/>'
+    data: '<defs><filter id="svgicon_arrow-left_a" x="432.594" y="412.125" width="10.781" height="19.75" filterUnits="userSpaceOnUse"><feFlood result="flood" flood-color="currentColor"/><feComposite result="composite" operator="in" in2="SourceGraphic"/><feBlend result="blend" in2="SourceGraphic"/></filter></defs><path pid="0" data-name="Forma 1" d="M432.888 422.694l8.8 8.888a.981.981 0 001.4 0 1 1 0 000-1.41l-8.1-8.183 8.1-8.183a1.006 1.006 0 000-1.412.984.984 0 00-1.4 0l-8.8 8.888a1.016 1.016 0 000 1.412z" transform="translate(-432.594 -412.125)" fill-rule="evenodd" filter="url(#svgicon_arrow-left_a)"/>'
   }
 })
--- components/icons/arrow-right.js
+++ components/icons/arrow-right.js
@@ -5,6 +5,6 @@ icon.register({
     width: 10.81,
     height: 19.813,
     viewBox: '0 0 10.81 19.813',
-    data: '<defs><filter id="svgicon_arrow-right_a" x="1088.63" y="412.125" width="10.81" height="19.813" filterUnits="userSpaceOnUse"><feFlood result="flood" flood-color="#fff"/><feComposite result="composite" operator="in" in2="SourceGraphic"/><feBlend result="blend" in2="SourceGraphic"/></filter></defs><path pid="0" data-name="Forma 1 copy 2" d="M1099.14 421.319l-8.82-8.9a.986.986 0 00-1.4 0 1 1 0 000 1.413l8.12 8.195-8.12 8.2a1 1 0 000 1.413.984.984 0 001.4 0l8.82-8.9a1.018 1.018 0 000-1.421z" transform="translate(-1088.63 -412.125)" fill-rule="evenodd" filter="url(#svgicon_arrow-right_a)"/>'
+    data: '<defs><filter id="svgicon_arrow-right_a" x="1088.63" y="412.125" width="10.81" height="19.813" filterUnits="userSpaceOnUse"><feFlood result="flood" flood-color="currentColor"/><feComposite result="composite" operator="in" in2="SourceGraphic"/><feBlend result="blend" in2="SourceGraphic"/></filter></defs><path pid="0" data-name="Forma 1 copy 2" d="M1099.14 421.319l-8.82-8.9a.986.986 0 00-1.4 0 1 1 0 000 1.413l8.12 8.195-8.12 8.2a1 1 0 000 1.413.984.984 0 001.4 0l8.82-8.9a1.018 1.018 0 000-1.421z" transform="translate(-1088.63 -412.125)" fill-rule="evenodd" filter="url(#svgicon_arrow-right_a)"/>'
   }
 })
